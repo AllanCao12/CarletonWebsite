a7639207a1b49dcbfa7b4789c2499207138f65e7
allancao1 <99683096+AllanCao12@users.noreply.github.com>
2024-07-22 00:25:23 -0400

    Added some stuff

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"), # '' is the home directory
+    path('clubEvents/', views.clubsEvents, name="clubEvents"),
+    path('major/', views.major, name="major"),
+    path('major/<str:pk>/', views.tips, name="majorAdvice")
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Major
 
 # Create your views here.
 
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
+
+def clubsEvents(request):
+    context = {}
+    return render(request, 'base/clubEvents.html', context)
+
+def major(request):
+    Majors = Major.objects.all()
+    context = {"Majors": Majors}
+    return render(request, 'base/majors.html', context)
+
+def tips(request):
+    context = {}
+    return render(request, 'base/tips.html', context)
